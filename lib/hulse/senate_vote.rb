92ea300f73d5a6a5faf725c2874826e57d93071e
Derek Willis <dwillis@gmail.com>
2014-01-01 17:31:30 -0500

    added senate vote year summary

--- lib/hulse/senate_vote.rb
+++ lib/hulse/senate_vote.rb
@@ -2,16 +2,16 @@ module Hulse
   class SenateVote
 
     attr_reader :congress, :session, :year, :vote_number, :vote_timestamp, :updated_at, :vote_question_text, :vote_document_text,
-    :vote_result_text, :question, :vote_title, :majority_requirement, :vote_result, :document, :amendment, :vote_count, :tie_breaker, :members
+    :vote_result_text, :question, :vote_title, :majority_requirement, :vote_result, :document, :amendment, :vote_count, :tie_breaker, :members, 
+    :vote_date, :issue
 
     def initialize(params={})
       params.each_pair do |k,v|
         instance_variable_set("@#{k}", v)
       end
     end
-
+    
     def self.find(year, vote)
-      # TODO: need to special-case 2013 votes from 112th congress, 2nd session
       congress, session = Hulse::Utils.convert_year_to_congress_and_session(year)
       url = "http://www.senate.gov/legislative/LIS/roll_call_votes/vote#{congress}#{session}/vote_#{congress}_#{session}_#{vote.to_s.rjust(5,"0")}.xml"
       response = HTTParty.get(url)
@@ -43,7 +43,14 @@ module Hulse
         members: members
       )
     end
-
-
+    
+    def self.summary(year)
+      congress, session = Hulse::Utils.convert_year_to_congress_and_session(year)
+      url = "http://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_#{congress}_#{session}.xml"
+      response = HTTParty.get(url)
+      votes = response.parsed_response['vote_summary']['votes']['vote']
+      votes.map{|v| self.new(congress: congress, session: session, year: year, vote_number: v['vote_number'], vote_date: v['vote_date'], issue: v['issue'], 
+        question: v['question'], vote_result: v['result'], vote_count: v['vote_tally'].inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}, vote_title: v['title'])}
+    end
   end
 end
