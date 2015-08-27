f6ac038c434cecaa8c4e149d5c852359556f5d94
Derek Willis <derek.willis@propublica.org>
2015-08-27 11:24:22 -0400

    added latest vote for year to SenateVote

--- lib/hulse/senate_vote.rb
+++ lib/hulse/senate_vote.rb
@@ -2,7 +2,7 @@ module Hulse
   class SenateVote
 
     attr_reader :congress, :session, :year, :vote_number, :vote_timestamp, :updated_at, :vote_question_text, :vote_document_text,
-    :vote_result_text, :question, :vote_title, :majority_requirement, :vote_result, :document, :amendment, :vote_count, :tie_breaker, :members, 
+    :vote_result_text, :question, :vote_title, :majority_requirement, :vote_result, :document, :amendment, :vote_count, :tie_breaker, :members,
     :vote_date, :issue
 
     def initialize(params={})
@@ -10,7 +10,14 @@ module Hulse
         instance_variable_set("@#{k}", v)
       end
     end
-    
+
+    def self.latest_vote(year)
+      congress, session = Hulse::Utils.convert_year_to_congress_and_session(year)
+      url = "http://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_#{congress}_#{session}.xml"
+      response = HTTParty.get(url)
+      response.parsed_response['vote_summary']['votes']['vote'].first['vote_number'].to_i
+    end
+
     def self.find(year, vote)
       congress, session = Hulse::Utils.convert_year_to_congress_and_session(year)
       url = "http://www.senate.gov/legislative/LIS/roll_call_votes/vote#{congress}#{session}/vote_#{congress}_#{session}_#{vote.to_s.rjust(5,"0")}.xml"
@@ -43,14 +50,14 @@ module Hulse
         members: members
       )
     end
-    
+
     def self.summary(year)
       congress, session = Hulse::Utils.convert_year_to_congress_and_session(year)
       url = "http://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_#{congress}_#{session}.xml"
       response = HTTParty.get(url)
       return nil if response.code == 404
       votes = response.parsed_response['vote_summary']['votes']['vote']
-      votes.map{|v| self.new(congress: congress, session: session, year: year, vote_number: v['vote_number'], vote_date: Date.strptime(v['vote_date']+"-#{year}", "%d-%b-%Y"), issue: v['issue'], 
+      votes.map{|v| self.new(congress: congress, session: session, year: year, vote_number: v['vote_number'], vote_date: Date.strptime(v['vote_date']+"-#{year}", "%d-%b-%Y"), issue: v['issue'],
         question: v['question'], vote_result: v['result'], vote_count: v['vote_tally'].inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}, vote_title: v['title'])}
     end
   end
