a53400c714340f57ab93e0d9725507db25d133b1
Derek Willis <derek.willis@propublica.org>
2015-12-03 15:48:48 -0500

    sponsorship class objects

--- lib/hulse/sponsorship.rb
+++ lib/hulse/sponsorship.rb
@@ -1,10 +1,16 @@
 module Hulse
   class Sponsorship
 
+    attr_reader :member_bioguide_id, :bill_number, :bill_url, :sponsor_bioguide_id, :congress, :date, :description
+
     def initialize(params={})
       params.each_pair do |k,v|
         instance_variable_set("@#{k}", v)
       end
+    end
+
+    def self.create_from_results(sponsorships)
+      sponsorships.map{|r| self.new(r)}
     end
 
     def self.base_url(congress)
@@ -27,49 +33,39 @@ module Hulse
     end
 
     def self.parse_html(html, bioguide_id, congress)
-      sponsored_bills = []
-      cosponsored_bills = []
+      sponsorships = []
       html.css('ol.results_list li h2').each do |bill|
         bill_num = bill.children.first.text
         bill_url = bill.children.first['href']
         sponsor_bioguide = bill.next.next.next.next.css('tr td').first.children.first['href'].split('/').last
         if sponsor_bioguide == bioguide_id
-          sponsored_bills << {bill: bill_num, url: bill_url, sponsor_bioguide: sponsor_bioguide, congress: congress}
+          sponsorships << {member_bioguide_id: bioguide_id, bill_number: bill_num, bill_url: bill_url, sponsor_bioguide_id: sponsor_bioguide, congress: congress, description: 'sponsored'}
         else
-          cosponsored_bills << {bill: bill_num, url: bill_url, sponsor_bioguide: sponsor_bioguide, congress: congress}
+          sponsorships << {member_bioguide_id: bioguide_id, bill_number: bill_num, bill_url: bill_url, sponsor_bioguide_id: sponsor_bioguide, congress: congress, description: 'cosponsored'}
         end
       end
-      [sponsored_bills, cosponsored_bills]
+      sponsorships
     end
 
     def self.member_details(url, congress)
-      sponsored_bills = []
-      cosponsored_bills = []
-      # each page has first 250 results; figure out pagination
       html = fetch(url+"?q=%7B%22congress%22%3A%22#{congress}%22%7D&pageSize=250")
       return [] if html.css('ol.results_list li h2').empty?
       total = html.css('strong').first.next.text.strip.split('of ').last.to_i
       max_page = (total.to_f/250.0).ceil
       bioguide_id = url.split('/').last
-      sp, csp = parse_html(html, bioguide_id, congress)
-      sponsored_bills << sp
-      cosponsored_bills << csp
+      sponsorships = parse_html(html, bioguide_id, congress)
       if max_page == 2
         doc = HTTParty.get(url+"?q=%7B%22congress%22%3A%22#{congress}%22%7D&pageSize=250&page=#{max_page}")
         html = Nokogiri::HTML(doc.parsed_response)
-        sp, csp = parse_html(html, bioguide_id, congress)
-        sponsored_bills << sp
-        cosponsored_bills << csp
+        sponsorships << parse_html(html, bioguide_id, congress)
       elsif max_page > 2
         (2..max_page).each do |page|
           doc = HTTParty.get(url+"?q=%7B%22congress%22%3A%22#{congress}%22%7D&pageSize=250&page=#{page}")
           html = Nokogiri::HTML(doc.parsed_response)
-          sp, csp = parse_html(html, bioguide_id, congress)
-          sponsored_bills << sp
-          cosponsored_bills << csp
+          sponsorships << parse_html(html, bioguide_id, congress)
         end
       end
-      [sponsored_bills.flatten, cosponsored_bills.flatten]
+      create_from_results(sponsorships.flatten)
     end
   end
 end
--- test/sponsorship_test.rb
+++ test/sponsorship_test.rb
@@ -5,12 +5,12 @@ module Hulse
   class SponsorshipTest < Minitest::Test
 
     def setup
-      @sponsored, @cosponsored = Sponsorship.member_details("https://www.congress.gov/member/joyce-beatty/B001281", "114")
+      @sponsorships = Sponsorship.member_details("https://www.congress.gov/member/joyce-beatty/B001281", "114")
     end
 
     def test_sponsored_bills
-      assert_equal @sponsored.size, 15
-      assert_equal @cosponsored.size, 275
+      assert_equal @sponsorships.select{|s| s.description == 'sponsored'}.size, 15
+      assert_equal @sponsorships.select{|s| s.description == 'cosponsored'}.size, 275
     end
 
   end
